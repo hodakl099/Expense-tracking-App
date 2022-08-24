ba194a86eb55cba9ba226ed63e8d1514ec7a103b
Mahmoud Alkawteb <mahmoud.alkateb22@gmail.com>
2022-08-24 17:42:06 +0200

    added the Adapter to the ViewPagerAdapter.kt

--- app/src/main/java/com/example/trackingapp/ui/HomeFragment.kt
+++ app/src/main/java/com/example/trackingapp/ui/HomeFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import com.example.trackingapp.R
 import com.example.trackingapp.databinding.FragmentHomeBinding
+import com.example.trackingapp.databinding.ItemCardBinding
+import com.example.trackingapp.ui.adapters.ViewPagerAdapter
 
 
 class HomeFragment : Fragment() {
@@ -14,6 +17,12 @@ class HomeFragment : Fragment() {
     private var _binding : FragmentHomeBinding? = null
 
     private val bindig get() = _binding!!
+
+    private var _bindingCard : ItemCardBinding? = null
+
+    private val bindingCard get() = _bindingCard!!
+
+    private var cardsList = mutableListOf<CardView>()
 
 
     override fun onCreateView(
@@ -25,5 +34,25 @@ class HomeFragment : Fragment() {
         val root = bindig.root
 
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        postToList()
+
+        bindig.viewPager2.adapter = ViewPagerAdapter(cardsList)
+
+
+    }
+
+    private fun addCardView(card : CardView) {
+        cardsList.add(card)
+    }
+
+    private fun postToList(){
+        for (i in 1..5) {
+            addCardView(bindingCard.cardViewItem)
+        }
     }
 }
--- app/src/main/java/com/example/trackingapp/ui/adapters/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/trackingapp/ui/adapters/ViewPagerAdapter.kt
@@ -1,24 +1,28 @@
 package com.example.trackingapp.ui.adapters
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import androidx.core.view.get
 import androidx.recyclerview.widget.RecyclerView
+import com.example.trackingapp.databinding.ItemCardBinding
 
-class ViewPagerAdapter(var listOfCards: MutableList<View>) : RecyclerView.Adapter<ViewPagerAdapter.Pager2ViewHolder>(){
+class ViewPagerAdapter(var listOfCards: MutableList<CardView>) : RecyclerView.Adapter<ViewPagerAdapter.Pager2ViewHolder>(){
 
-    inner class Pager2ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    inner class Pager2ViewHolder(var binding: ItemCardBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun getItemCount(): Int {
         return listOfCards.size
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Pager2ViewHolder {
-        TODO("Not yet implemented")
+        val binding = ItemCardBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+
+        return Pager2ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: Pager2ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.binding.cardViewItem[position]
     }
-
-
 }
