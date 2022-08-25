105791e9819983ea5e253f3c194b6983995d169f
Mahmoud Alkateb <mahmoud.alkateb22@gmail.com>
2022-08-25 07:46:03 +0200

    added the GoalAdapter RecyclerView to ExpenseFragment.kt

--- app/src/main/java/com/example/trackingapp/ui/ExpenseFragment.kt
+++ app/src/main/java/com/example/trackingapp/ui/ExpenseFragment.kt
@@ -5,17 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.trackingapp.R
+import com.example.trackingapp.databinding.FragmentExpenseBinding
+import com.example.trackingapp.databinding.GoalItemBinding
+import com.example.trackingapp.ui.adapters.GoalsAdapter
+import com.example.trackingapp.ui.data.GoalItem
 
 
 class ExpenseFragment : Fragment() {
+
+   private lateinit var binding: FragmentExpenseBinding
+
+   private lateinit var goalAdapter: GoalsAdapter
+
+   private var listOfGoals : MutableList<GoalItem>? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_expense, container, false)
+        val binding = GoalItemBinding.inflate(layoutInflater,container,false)
+        listOfGoals?.add(GoalItem("Home","100%"))
+        goalAdapter = GoalsAdapter(listOfGoals)
+
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
     }
 
 }
--- app/src/main/java/com/example/trackingapp/ui/adapters/GoalsAdapter.kt
+++ app/src/main/java/com/example/trackingapp/ui/adapters/GoalsAdapter.kt
@@ -1,10 +1,32 @@
 package com.example.trackingapp.ui.adapters
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.trackingapp.databinding.GoalItemBinding
 import com.example.trackingapp.ui.data.GoalItem
 
-class GoalsAdapter(private val dataSet: MutableList<GoalItem>) : RecyclerView.Adapter<GoalsAdapter.GoalViewHolder>() {
+class GoalsAdapter(private val dataSet: MutableList<GoalItem>?) : RecyclerView.Adapter<GoalsAdapter.GoalViewHolder>() {
 
-    inner class GoalViewHolder()
+    inner class GoalViewHolder(val binding: GoalItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun getItemCount(): Int {
+        return dataSet?.size ?: 0
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GoalViewHolder {
+        val binding = GoalItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return GoalViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: GoalViewHolder, position: Int) {
+        val currentGoalItem = dataSet?.get(position)
+
+        holder.binding.apply {
+            tvGoalName.text = currentGoalItem?.GoalCategory
+            tvGoalPercentage.text = currentGoalItem?.GoalPercentage
+        }
+    }
+
 
 }
