86c212b9b4d53c00440bd0a6748b35d6408e53f1
Mahmoud Alkawteb <mahmoud.alkateb22@gmail.com>
2022-08-29 16:37:54 +0200

    nav_graph.xml

--- app/src/main/java/com/example/trackingapp/ui/ui/MainActivity.kt
+++ app/src/main/java/com/example/trackingapp/ui/ui/MainActivity.kt
@@ -2,7 +2,13 @@ package com.example.trackingapp.ui.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.viewpager2.widget.ViewPager2
+import com.example.trackingapp.R
 import com.example.trackingapp.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayout
 
@@ -10,9 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private lateinit var tabLayout: TabLayout
-
-    private lateinit var viewPager2: ViewPager2
+    private lateinit var navController : NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
