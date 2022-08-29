4b0a82e24092a077b2d4c3f6357dd89dc4501017
Mahmoud Alkateb <mahmoud.alkateb22@gmail.com>
2022-08-29 10:44:43 +0200

    the date calendar is set AddIncomeFragment.kt

--- app/src/main/java/com/example/trackingapp/ui/ui/AddIncomeFragment.kt
+++ app/src/main/java/com/example/trackingapp/ui/ui/AddIncomeFragment.kt
@@ -1,13 +1,16 @@
 package com.example.trackingapp.ui.ui
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.CalendarView
 import com.example.trackingapp.R
 import com.example.trackingapp.databinding.FragmentAddIncomeBinding
+import java.util.*
 
 
 class AddIncomeFragment : Fragment() {
@@ -40,6 +43,20 @@ class AddIncomeFragment : Fragment() {
         )
         binding.tvAutoCompletePaymentText.setAdapter(arrayPaymentAdapter)
 
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        binding.tvDateText.setOnClickListener{
+            val dpd = DatePickerDialog(requireContext(), DatePickerDialog.OnDateSetListener{view,mYear, mMonth, mDay ->
+
+                binding.tvDateText.text = "$mDay/${mMonth + 1}/$mYear"
+
+            }, year, month, day)
+
+            dpd.show()
+        }
 
         return binding.root
     }
