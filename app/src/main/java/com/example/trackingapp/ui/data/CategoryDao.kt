5bf82697f8b67cf2aade73be99eed6ba097abf60
Mahmoud Alkateb <mahmoud.alkateb22@gmail.com>
2022-09-17 23:04:48 +0200

    implementing Room Database

---
+++ app/src/main/java/com/example/trackingapp/ui/data/CategoryDao.kt
@@ -0,0 +1,26 @@
+package com.example.trackingapp.ui.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.IGNORE
+import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.Query
+
+@Dao
+interface CategoryDao {
+
+    @Query("SELECT * FROM category_table")
+    fun getAll() : List<Category>
+
+    @Insert(onConflict = REPLACE)
+    suspend fun insert(category: Category)
+
+    @Delete
+    suspend fun delete(category: Category)
+
+    @Query("DELETE  FROM category_table ")
+    suspend fun deleteAll()
+
+
+}
