b043de2c1365ab443f4fc9e7dd2114cbbaddd3d6
Mahmoud Alkawteb <mahmoud.alkateb22@gmail.com>
2022-10-01 14:38:59 +0200

    implemented the for each iterator to add the transaction to the list

--- app/src/main/java/com/example/trackingapp/ui/ui/TransactionFragment.kt
+++ app/src/main/java/com/example/trackingapp/ui/ui/TransactionFragment.kt
@@ -9,13 +9,10 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.example.trackingapp.databinding.FragmentTransactionBinding
 import com.example.trackingapp.ui.adapters.TransactionAdapter
-import com.example.trackingapp.ui.data.AmountTransaction
 import com.example.trackingapp.ui.data.Transaction
-import com.example.trackingapp.ui.viewmodel.SharedViewModel
 import com.example.trackingapp.ui.viewmodel.TransactionViewModel
 import java.text.DecimalFormat
 import java.util.*
-import kotlin.math.absoluteValue
 
 
 class TransactionFragment : Fragment() {
@@ -23,13 +20,11 @@ class TransactionFragment : Fragment() {
 
     private lateinit var binding: FragmentTransactionBinding
 
-    private lateinit var transactionViewModel: TransactionViewModel
-
     private val list = mutableListOf<Transaction>()
 
     private lateinit var recyclerView : RecyclerView
 
-
+    private lateinit var transactionViewModel: TransactionViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,31 +37,28 @@ class TransactionFragment : Fragment() {
 
         transactionViewModel = ViewModelProvider(requireActivity())[TransactionViewModel::class.java]
 
-        transactionViewModel.getExpense.observe(viewLifecycleOwner) { data ->
 
 
-            data.listIterator().forEach {
-                //if there is element value equals 00.0 as a default value it will return the for each list iterator.
-                if (it.Expense == 00.0) {
-                    return@forEach
-                }else {
+        transactionViewModel.getExpense.observe(viewLifecycleOwner){ data ->
+            data.listIterator().forEach { expense ->
+
+                //if the its the default value which is 00.0 then return the foreach list iterator.
+                if (expense.Expense == 00.0) return@forEach
+                else {
                     //format expense
                     val decExpense = DecimalFormat("#,###.##")
-                    val numberExpense = java.lang.Double.valueOf(it.Expense)
+                    val numberExpense = java.lang.Double.valueOf(expense.Expense)
                     val valueExpense = decExpense.format(numberExpense)
                     val currency = Currency.getInstance("USD")
-                    val symbol= currency.symbol
-                    val formatExpense = String.format("$symbol$valueExpense","%.2f" )
-                    addTransaction(formatExpense)
+                    val symbol = currency.symbol
+                    val formattedExpense = String.format("$symbol$valueExpense", "%.2f")
+
+                    addTransaction(formattedExpense)
                 }
 
             }
 
-
-
-
         }
-
 
         val transactionAdapter = TransactionAdapter(list)
 
@@ -80,9 +72,9 @@ class TransactionFragment : Fragment() {
 
 
     }
-
     private fun addTransaction(data : String) {
         list.add(Transaction(data))
     }
+
 
 }
