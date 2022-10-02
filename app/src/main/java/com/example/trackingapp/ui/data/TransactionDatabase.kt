842b9530b0a27cb6524f7d9d9ac0452543d97fec
Mahmoud Alkateb <mahmoud.alkateb22@gmail.com>
2022-10-02 15:45:32 +0200

    sat the schemaExport in database to false

--- app/src/main/java/com/example/trackingapp/ui/data/TransactionDatabase.kt
+++ app/src/main/java/com/example/trackingapp/ui/data/TransactionDatabase.kt
@@ -5,8 +5,9 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Transaction::class], version = 1,
-    exportSchema = true,
+@Database(entities = [Transaction::class],
+    version = 1,
+    exportSchema = false,
     )
 abstract class TransactionDatabase : RoomDatabase() {
 
